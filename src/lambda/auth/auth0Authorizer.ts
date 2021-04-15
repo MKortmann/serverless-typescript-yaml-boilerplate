b99707d17a7ab700f2dfa02a75ed6e62ede5d84f
MKortmann <companymvms@gmail.com>
2021-04-15 09:43:19 +0200

    add auth0Authorizer with KMS and AWS SecretsManager

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,5 +1,7 @@
 import { APIGatewayTokenAuthorizerEvent, APIGatewayAuthorizerResult, APIGatewayAuthorizerHandler } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
 // import * as middy from 'middy'
 // import { secretsManager } from 'middy/middlewares'
 
@@ -10,14 +12,23 @@ import { JwtToken } from '../../auth/JwtToken'
 
 const auth0Secret = process.env.AUTH_0_SECRET_ID;
 
-// const secretId = process.env.AUTH_0_SECRET_ID
-// const secretField = process.env.AUTH_0_SECRET_FIELD
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+// used to read our Auth0 Secret
+const client = new AWS.SecretsManager()
+
+// Cache secret if a Lambda instance is reused, and do not sent requests to
+// secrets manager over and over again, lambda function it will be at least some minutes more in
+// the memory allowing to reuse it. It save us some money on calling AWS Secret Manager
+let cachedSecret: string
 
 export const handler: APIGatewayAuthorizerHandler = async (event: APIGatewayTokenAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
 
   try {
     // we first call the verifyToken function and pass the token that we need to verify.
-    const decodedToken = verifyToken(event.authorizationToken, auth0Secret)
+    // we use here await to convert the promise into a value
+    const decodedToken = await verifyToken(event.authorizationToken)
 
     console.log('User was authorized')
 
@@ -58,7 +69,9 @@ export const handler: APIGatewayAuthorizerHandler = async (event: APIGatewayToke
   }
 }
 
-function verifyToken(authHeader: string, secret: string): JwtToken {
+// because of await it should be async function.
+// async function returns a promise
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader)
     throw new Error('No authentication header')
 
@@ -68,20 +81,28 @@ function verifyToken(authHeader: string, secret: string): JwtToken {
   const split = authHeader.split(' ')
   const token = split[1]
 
+  const secretObject: any = await getSecret()
+
+  const secret = secretObject[secretField]
+
   // no acception, so a request has been authorized
   // verify the function and return the result as JwtToken (cast operation)
   return verify(token, secret) as JwtToken
 }
 
-// handler.use(
-//   secretsManager({
-//     awsSdkOptions: { region: 'us-east-1' },
-//     cache: true,
-//     cacheExpiryInMillis: 60000,
-//     // Throw an error if can't read the secret
-//     throwOnFailedCall: true,
-//     secrets: {
-//       AUTH0_SECRET: secretId
-//     }
-//   })
-// )
+// we call the cachedSecret here
+async function getSecret() {
+
+  if(cachedSecret) return cachedSecret
+
+  const data = await client.getSecretValue({
+    SecretId: secretId
+  })
+  .promise()
+
+  cachedSecret = data.SecretString
+
+  return JSON.parse(cachedSecret)
+
+
+}
